c45f390eaa870f38e620397b00d14d8179b6612d
vdevcode <thanhvumaidao1234567@gmail.com>
2024-01-17 14:47:36 +0700

    change host

--- be-client/src/pages/home/ListStudent.jsx
+++ be-client/src/pages/home/ListStudent.jsx
@@ -27,10 +27,7 @@ const AdminDashboard = () => {
   });
 
   useEffect(() => {
-    // Khởi tạo userData trước khi fetch
     setUserData([]);
-
-    fetchData(); // Gọi fetchData để đảm bảo dữ liệu mới nhất
   }, [searchParams]);
 
   console.log(setUserData);
@@ -88,21 +85,22 @@ const AdminDashboard = () => {
     setSortOption(e.target.value);
   };
 
-
   const sendDataToGoogleSheets = async (data) => {
     try {
-      const response = await axios.post('https://script.google.com/macros/s/AKfycbzCdDZ7T5o2_3kY3bVNNgHKajOdv471DzWdRICIuiz3GA-igCKDPKr3yl0Sfpwo-LoJxQ/exec', data, {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
+      const response = await axios.post(
+        "https://script.google.com/macros/s/AKfycbzCdDZ7T5o2_3kY3bVNNgHKajOdv471DzWdRICIuiz3GA-igCKDPKr3yl0Sfpwo-LoJxQ/exec",
+        data,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
       console.log(response.data); // In thông báo từ script (Success)
     } catch (error) {
-      console.error('Error sending data to Google Sheets:', error);
+      console.error("Error sending data to Google Sheets:", error);
     }
   };
-  
-  
 
   return (
     <div className="bg-banner-login bg-center bg-no-repeat bg-cover max-w-screen-2xl py-4 container mx-auto px-4 lg:px-24 ">
@@ -192,8 +190,8 @@ const AdminDashboard = () => {
                 name: searchParams.name,
                 address: searchParams.address,
                 phone: searchParams.phone,
-                email: searchParams.email, 
-                majors: searchParams.major, 
+                email: searchParams.email,
+                majors: searchParams.major,
               })
             }
             className="my-4 text-white mt-3 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
@@ -287,7 +285,6 @@ const AdminDashboard = () => {
                 Trang Trước
               </button>
 
-              {/* Ví dụ về nút "Trang Sau" */}
               <button
                 onClick={() => handlePageChange(searchParams.page + 1)}
                 className="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
