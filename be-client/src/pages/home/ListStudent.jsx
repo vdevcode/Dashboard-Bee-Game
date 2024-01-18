d65078f05005214c3c71c686cc86666d59d5f284
vdevcode <thanhvumaidao1234567@gmail.com>
2024-01-18 20:59:31 +0700

    upload fe

--- be-client/src/pages/home/ListStudent.jsx
+++ be-client/src/pages/home/ListStudent.jsx
@@ -196,7 +196,7 @@ const AdminDashboard = () => {
                 majors: searchParams.major, 
               })
             }
-            className="my-4 text-white mt-3 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="my-4 h-[40px] text-white mt-3 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Đẩy dữ liệu lên Google Sheets
           </button>
@@ -204,7 +204,7 @@ const AdminDashboard = () => {
           <a
             href="http://167.71.196.197/api/user/download"
             onClick={() => document.getElementById("my_modal_2").showModal()}
-            className="mb-2 ml-0 md:ml-4 text-white mt-3 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="mb-2 h-[40px] ml-0 md:ml-4 text-white mt-3 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Tải File Excel
           </a>
